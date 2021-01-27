f6ee5c2b7f6951e9c96c38fe605aae3350867b02
LiniiS <enila404@gmail.com>
2021-01-27 16:00:21 -0300

    Adicionado pagina quiz, questões, ajuste no button

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
+import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
+import Button from '../src/components/Button';
 import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizLogo from '../src/components/QuizLogo';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-/*
-const BackgroundImage = styled.div`
-  background-image:url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
- `;
-*/
-
-const Button = styled.button`
-  background-color: #001970;
-  border: 2px;
-  border-radius: 3px;
-  box-shadow: 0px 2px 2px darkblue;
-  color: white;
-  display: inline-block;
-  margin-top: 15px;
-  outline: 0;
-  padding: 5px 15px;
-  width: 100%;
-
-`;
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and(max-width: 500px){
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
 
 export default function Home() {
   //por regra o hook fica aqui
@@ -68,18 +38,14 @@ export default function Home() {
               //next cuida do router das paginas
               }}
             >
-              <input
-                onChange={function (infosDoEvento) {
-                  console.log(infosDoEvento.target.value);
-                  //State a ser observado
-                  //name = infosDoEvento.target.value;
-                  setName(infosDoEvento.target.value);
-                }}
-
-                placeholder="digite seu nome!" />
+              <Input
+                name="nomeDoJogador"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="digite seu nome para jogar"
+                value={name}
+              />
               <Button type="submit" disabled={name.length===0}>
-                Jogar
-                {name}
+                {`Jogar ${name}`}              
               </Button>
             </form>
           </Widget.Content>
